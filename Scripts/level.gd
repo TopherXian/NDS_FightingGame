2c3edc3fb6dc0eec43a466be232acfbcf6b5310b
Cristopher-Artacho-WVSU <cristopherian.artacho@wvsu.edu.ph>
2025-04-17 08:00:40 +0800

    Added Timer on Level

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+@onready var label = $TimerLabel
+@onready var timer = $Timer
+
+func _ready():
+	timer.start()
+	
+func time_left():
+	var time = timer.time_left
+	return time
+
+func _process(delta):
+	label.text = "%02d" % int(time_left())
